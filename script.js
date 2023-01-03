f5327c26cbcc33a92efb0654aa2be201008f2637
mohammadyousefvand <www.mmhmdmm83@gmail.com>
2023-01-03 16:40:09 +0330

    fix: add comment

--- script.js
+++ script.js
@@ -1,20 +1,34 @@
 const pickerButton = document.querySelector(".picker-btn")
 const colorsList = document.querySelector(".color-list")
 const clearAllBtn = document.querySelector(".clear-color")
+
+// if data in localStorage get in initial run code
 const colorsPickedList = JSON.parse(localStorage.getItem("picked-color") || "[]")
 
-
+// convert rgba to hex 
 const rgbaToHex = (rgba) => {
     const rgb = rgba.substring(5, rgba.length - 1).split(",")
     return "#" + ((1 << 24) + (+rgb[0] << 16) + (+rgb[1] << 8) + +rgb[2]).toString(16).slice(1);
 }
 
+// when click to color item color copied
 const copyColor = (element) => {
-    navigator.clipboard.writeText(element.dataset.color)
+    copyText(element.dataset.color)
+    // change element text after copy and reset with settimeout
     element.innerHTML = "Copied"
     setTimeout(() => element.innerHTML = element.dataset.color, 500);
 }
 
+const copyText = (text) => {
+    // check the browser to support navigator api
+    if (window.navigator) {
+        navigator.clipboard.writeText(text)
+    } else {
+        console.log("Yor browser does not support navigator api");
+    }
+}
+
+// create and update ui after all change
 const createUiColorList = () => {
     const colorItem = colorsPickedList.map(color => {
         return `
@@ -26,31 +40,40 @@ const createUiColorList = () => {
     }).join("")
     colorsList.innerHTML = colorItem
 
+    // set navigator copy to all item color in color list
     colorsList.querySelectorAll("li").forEach(item => {
         item.addEventListener("click", (e) => {
             copyColor(e.currentTarget.lastElementChild)
         })
     })
 }
+// initial create ui with localStorage data
 createUiColorList()
 
+// clear all color list and update localStorage  
 const clearAllColor = () => {
     colorsPickedList.length = 0
     localStorage.setItem("picked-color", JSON.stringify(colorsPickedList))
     createUiColorList()
 }
 
+// handel picked color with EyeDropper api 
+// EyeDropper api does not supported in all browsers just chrome, opera and edge
 const pickedColor = async () => {
     document.body.style.display = "none"
+    // remove body after clicked button picker and open Eyedropper tool to target color under the body
     setTimeout(async () => {
         try {
             const eyeDraper = new EyeDropper()
+            // destructure color from Dropper api
             const { sRGBHex } = await eyeDraper.open()
+            // send value to convert rgba to hex function
             const convertToHex = rgbaToHex(sRGBHex)
-            navigator.clipboard.writeText(convertToHex)
+            copyText(convertToHex)
             if (!colorsPickedList.includes(convertToHex)) {
                 colorsPickedList.push(convertToHex)
             }
+            // save color picked in localStorage and update ui
             localStorage.setItem("picked-color", JSON.stringify(colorsPickedList))
             createUiColorList()
         } catch (error) {
@@ -60,6 +83,7 @@ const pickedColor = async () => {
     }, 10);
 }
 
+// Event handler for picked color and clear color list
 pickerButton.addEventListener("click", pickedColor)
 clearAllBtn.addEventListener("click", clearAllColor)
 
